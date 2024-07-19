6cfa627fefc480b25e06eb21b5e0a8c680333d39
Lucy <sonyelove@gmail.com>
2024-07-19 23:39:34 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,25 @@
+let word_string = 
+`1. 기본동사 - Take
+제가 이 팜플릿 가져가도 될까요?!Can I take this pamphlet?
+나는 평화로를 타요.!I take the Peace Road.
+난 그 영어 수업 들어요.!I take the English class.
+제가 그 일을 맡기로 마음먹었습니다.!I decided to take the job.
+아스피린 좀 먹어요.!Take some aspirin.
+저 좀 공항에 데려다 주세요.!Please take me to the airport.
+여권 꺼내세요. (여권 좀 보여주세요.)!Take out your passport.
+그녀는 점심을 먹으러 그를 데리고 나갔다.!She took him out for lunch.
+그 태풍이 모든 것을 앗아갔다.!The typhoon took everything away.
+나는 이 회사를 인수할 것이다.!I will take over this company.
+모자를 벗어요.!Take off your hat.
+그는 5시에 출발했어요.!He took off at five.
+택시 타고 갑시다.!Let's take a taxi.
+그 서류 한 번 봐 주세요.!Take a look at the document.
+한 모금 마셔 봐요.!Take a sip.
+깊게 숨을 쉬어 봐요.!Take a deep breath.
+저는 이번 기회를 잡을 거예요.!I will take this opportunity.
+한번 시도 해 봐요.!Take a shot.
+이거 잘 좀 부탁드립니다.!Please take good care of this.
+산책하자!!Let's take a walk!
+좀 쉬어요.!Take a rest.
+
+`;
