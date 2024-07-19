8f094541fd68e0bd88e84b7261015388d7b1b04a
Lucy <sonyelove@gmail.com>
2024-07-20 00:31:53 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,25 +1,170 @@
 let word_string = 
-`1. 기본동사 - Take
-제가 이 팜플릿 가져가도 될까요?!Can I take this pamphlet?
-나는 평화로를 타요.!I take the Peace Road.
-난 그 영어 수업 들어요.!I take the English class.
-제가 그 일을 맡기로 마음먹었습니다.!I decided to take the job.
-아스피린 좀 먹어요.!Take some aspirin.
-저 좀 공항에 데려다 주세요.!Please take me to the airport.
-여권 꺼내세요. (여권 좀 보여주세요.)!Take out your passport.
-그녀는 점심을 먹으러 그를 데리고 나갔다.!She took him out for lunch.
-그 태풍이 모든 것을 앗아갔다.!The typhoon took everything away.
-나는 이 회사를 인수할 것이다.!I will take over this company.
-모자를 벗어요.!Take off your hat.
-그는 5시에 출발했어요.!He took off at five.
-택시 타고 갑시다.!Let's take a taxi.
-그 서류 한 번 봐 주세요.!Take a look at the document.
-한 모금 마셔 봐요.!Take a sip.
-깊게 숨을 쉬어 봐요.!Take a deep breath.
-저는 이번 기회를 잡을 거예요.!I will take this opportunity.
-한번 시도 해 봐요.!Take a shot.
-이거 잘 좀 부탁드립니다.!Please take good care of this.
-산책하자!!Let's take a walk!
-좀 쉬어요.!Take a rest.
+`Day1-2. 건강유지, 식습관
+건강을 유지하다 !stay healthy
+운동하다 !work out
+최선을 다하다 !try one’s best
+산책을 하다 !take a walk
+달리기 조깅을 하다 !go for a run
+제대로 잘 먹다 !eat well and properly
+규칙적으로 생활하다 !follow a regular lifestyle
+충분히 자다 !get enough sleep
+정신 건강 !mental health
+긍정적으로 생각하다 !stay positive
+밝은 면을 보다 !look on the bright side
+개인위생 !personal hygiene
+손을 잘 씻는다. !wash one's hands well
+마스크를 착용하다 !wear a face mask
+건강을 의식하는 !health-conscious
+보다 건강하게 먹다 !eat healthier
+그러한 이유로 !for that reason
+건강하게 먹다 !eat healthy
+건강하게 먹는 사람 !healthy eater
+선택하다 !opt for
+무첨가 식품 !whole foods
+가공식품 !processed foods
+유기농 식품 !organic food
+장을 보다 !get groceries
+음식을 팔다 !serve food
+~할 가능성이 더 높다 !be more likely to
+충성도 높은 고객 !loyal customer
+건강식 !healthy options
+
+Day3-4. 건강검진, 개인위생
+건강검진 !medical check-up
+금식을 하다 !go on a fast
+위/대장 내시경을 받다 !get a gastroscopy / colonoscopy procedure
+신체 측정 !body measurement
+폐활량 !lung capacity
+혈액 샘플을 채취하다 !have a blood sample taken
+제출하다 !turn in
+소변 샘플 !urine sample
+정밀 검사 !in-depth test
+초음파 검사를 받다 !get an ultrasound
+대변을 보다 !move one's bowels
+내시경 검사를 받다 !get an endoscopy
+수면 마취를 하다 !put someone under
+검토하다 !examin
+시험/검사 결과를 받다 !receive test results
+고혈압 !high blood pressure
+건강증명서, 진단 !clean bill of health
+코로나바이러스 대유행 !coronavirus pandemic
+개인위생 !personal hygiene
+중대 사안 !critical issue
+실감하다 !realize
+손을 씼다 !wash one's hands
+무의식적으로 !without even thinking about it
+병균을 퍼뜨리다 !spread germs
+호흡 기관 !respiratory system
+제거하다 !get rid of
+중요한/핵심부분 !an integral part of
+보편적 대세로 자리 잡았다 !have become the norm
+예절 !etiquette
+기침 !coughing
+분비물 !secretion
+병원균을 포함하다 !contain pathogens
+옷 소매로 입을 가리다 !cover one's mouth with one's sleeve
+챙기다, 배려하다 !look out for
+
+Day5-6. 체중조절, 치아관리
+~의 주요원인 !the leading cause of
+만성질환 !chronic illness
+심장질환 !heart disease
+당뇨병 !diabetes
+건강한 체중을 유지하다 !maintain a healthy weight
+복부 지방 !belly fat
+식단 조절을 하다 !watch what you eat
+기초 대사량 !basal metabolism
+살이 찌다 !put on pounds
+식단 조절을 하다 !control one's diet
+결국 ~하게 되다 !be bound to
+체중이 늘다 !gain weight
+규칙적인 운동 !regular exercise
+열량을 소모하다 !burn calories
+과도한 다이어트 !excessive diet
+생활 습관 !lifestyle habits
+치통이 생기다 !get a toothache
+건강에 악영향을 미치다 !take a toll on one's health
+~에 통증이 있다 !have pain in
+~에 영향을 끼치다 !have an effect on
+외모 !physical appearance
+입 냄새 !bad breath
+~을 잘 관리하다 !take good care of
+이를 닦다 !brush one's teeth
+최대한 자주 !as often as possible
+치실질을 하다 !floss one's teeth
+치과에 가다 !visit a dental clinic
+스케일링을 받다 !get one's teeth cleaned
+치석 !plaque
+
+Day7-8. 시력, 허리통증
+나쁜 시력 !poor eyesight /bad vision
+안경을 쓰다 !wear glasses
+렌즈를 끼우다 !wear contacts
+일회용 렌즈 !disposable contacts
+여부를 고민해보다 !debate whether
+시력 교정술 !eye correction surgery
+눈을 쉬게하다 !rest one's eyes
+매우 좋은 시력 !20/20 vision
+눈이 건조해지다 !eyes get dried out
+장시간 동안 !for an extended period of time
+안약을 넣다 !apply eye drops
+~을 가까운 곳에 두다 !keep ~ within arm's reach
+충혈되다 !become bloodshot
+안구 염증 !eye infection
+조심하다 !be careful
+요통을 호소하다 !complain of back problems
+만성요통 !chronic back pain
+정상적인 생활을 하다 !function normally
+간단하게 치료되다 !be treated simply
+~을 사전에 예방하다 !prevent ~ in advance
+바른 자세를 유지하다 !maintain good posture
+구부정한 자세를 하다 !slouch over
+~에 무리를 주다 !put a load on
+좋지 않다 !be no good
+측만증 !scoliosis
+비정상적인 방향으로 휘다 !curve the wrong way
+닳다 !wear out
+디스크 증세 !herminated disc
+심한 통증 !severe pain
+수술을 필요로 하다 !require surgery
+
+Day9-10. 어깨결림, 암
+결린 어깨 !stiff shoulders
+충분한 운동을 하다 !get enough exercise
+앉아 있다 !stay seated
+장기간 동안 !for long periods of time
+생할 습관 !lifestyle habits
+나쁜 자세 !bad posture
+어찌 보면 !in a way
+만성 질환 !chronic affliction
+무리한 운동 !excessive exercise
+~의 결림 !a stitch in
+지속되다 !go on
+오십견 !frozen shoulder
+~의 염증 !an inflammation of
+연결 조직 !connective tissue
+만성 결림 !chronic stiffness
+제한된 움직임 !limited movement
+심한 통증 !severe pain
+오랫동안 지속되다 !last for a long time
+쉽게 치료할 방법이 없다 !there's no easy cure
+근본적인 해결책 !fundamental solution
+의료진의 도움을 구하다 !seek medical aid
+꾸준히 운동하다 !exercise regularly
+좋은 자세를 유지하다 !maintain good posture
+위협을 가하다 !pose a threat
+암 유형 !form of cancer
+~순으로 !in order of
+크게 발전하다 !have come a long way
+화학 항암 치료 !chemo
+수술을 받다 !undergo surgery
+5년 생존율 !five-year survival rate
+가족력 !family history
+~에 더 집중하다/신경을 쓰다 !pay closer attention to
+조기진단 !early diagnosis
+정기적인 검사 !regular check-up
+큰 충격이다 !come as a shock
+생활 습관 !lifestyle habits
+유발 요인 !trigger
 
 `;
